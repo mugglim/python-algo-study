c373c3738bb2a5511c6178c03684c41549d1b70f
Jaeuk Lim <applewodnr@naver.com>
2021-11-30 18:45:03 +0900

    modify lc-hash-706

--- 해시테이블/lc-hash-706.py
+++ 해시테이블/lc-hash-706.py
@@ -4,10 +4,10 @@ class MyHashMap:
         self.data = {}
 
     def put(self, key: int, value: int) -> None:
-        self.data[key] = value;
-`
+        self.data[key] = value
+
     def get(self, key: int) -> int:
         return -1 if key not in self.data else self.data[key]
 
     def remove(self, key: int) -> None:
-        if key in self.data: del self.data[`key]
+        if key in self.data: del self.data[key]
