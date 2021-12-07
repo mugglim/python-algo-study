4c9d5802ed6a86e9887e0d7a00eec2133c64df8a
Jaeuk Lim <applewodnr@naver.com>
2021-12-07 19:00:14 +0900

    comment: lc-array-15 파일 테스트를 위한 코드 주석 처리

--- 배열/lc-array-15.py
+++ 배열/lc-array-15.py
@@ -44,12 +44,12 @@ class Solution:
                     if key not in result: result[key] = True
 
         return [[*k] for k in result.keys()]
+
 # for testing
-tc = [
-    [-1,0,1,2,-1,-4],
+# tc = [
+    # [-1,0,1,2,-1,-4],
     # [-1,0,1,2,-1,-4,-2,-3,3,0,4],
     # [34,55,79,28,46,33,2,48,31,-3,84,71,52,-3,93,15,21,-43,57,-6,86,56,94,74,83,-14,28,-66,46,-49,62,-11,43,65,77,12,47,61,26,1,13,29,55,-82,76,26,15,-29,36,-29,10,-70,69,17,49]
 ]
 
-for l in tc:
-    print(Solution().threeSum(l))
+# for l in tc: print(Solution().threeSum(l))
