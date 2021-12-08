861dabf8d7bd70325f8357e304e69b4fbf239198
Jaeuk Lim <applewodnr@naver.com>
2021-12-08 14:59:29 +0900

    docs: 투포인터 설명에 for loop 사용 방법 추가

--- docs/two-pointer.py
+++ docs/two-pointer.py
@@ -1,15 +1,44 @@
-def twoPointer(arr, target):
+import time
+
+# while loop
+def twoPointerWithWhileLoop(arr, target):
     ans = -1
     l, r = 0, len(arr) -1
 
     while(l < r):
-        total = a[l] + a[r]
+        total = arr[l] + arr[r]
         if total == target:
-            ans = [l, r]
+            ans = [arr[l], arr[r]]
             break
-        elif total < target:
-            l += 1
-        elif total > target:
-            r -= 1
+        if total < target: l += 1
+        if total > target: r -= 1
 
     return ans
+
+# for loop
+def twoPointerWithForLoop(arr, target):
+    ans = -1
+    length = len(arr)
+    r = 0
+
+    for l in range(length):
+        while r < length and arr[r] + arr[l] < target: r += 1
+        if r == length: break
+        if arr[r] + arr[l] >= target:
+            if arr[r] + arr[l] == target: ans = [arr[l], arr[r]]
+            break
+
+    return ans
+
+
+def getTime(cb, args):
+    st = time.time()
+    cb(*args)
+    return time.time() - st
+
+
+TARGET = int(1e7)
+arr = [x for x in range(TARGET + 1)]
+
+print(getTime(twoPointerWithWhileLoop, [arr,TARGET // 2])) # 1.517s
+print(getTime(twoPointerWithForLoop, [arr,TARGET // 2])) # 0.96s
