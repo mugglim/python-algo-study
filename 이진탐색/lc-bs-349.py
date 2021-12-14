a8b50e1fdcce1e3cd6c928b47d747097f1ac41df
Jaeuk Lim <applewodnr@naver.com>
2021-12-14 11:21:16 +0900

    add: lc-bs-349

---
+++ 이진탐색/lc-bs-349.py
@@ -0,0 +1,24 @@
+import bisect
+
+def binarySearch(arr, target):
+    idx = bisect.bisect_left(arr, target)
+    return 0 <= idx < len(arr) and arr[idx] == target
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # (1) 이진탐색
+        dic = {}
+
+        nums1.sort()
+        nums2.sort()
+
+        for n in nums1:
+            isExist = binarySearch(nums2, n)
+            if n not in dic and isExist: dic[n] = True
+
+        return dic.keys()
+
+        # (2) set 자료구조 이용
+        # nums1,nums2 = set(nums1), set(nums2)
+        # return list(nums1.intersection(nums2))
