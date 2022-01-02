570008b42841687e916268259412c0cc32dfc291
Jaeuk Lim <applewodnr@naver.com>
2022-01-02 14:27:49 +0900

    sol: boj-imp-1713

---
+++ categories/implementation/boj-imp-1713.py
@@ -0,0 +1,42 @@
+import sys
+from heapq import heappop, heappush, heapify
+
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+recommends = list(map(int,input().split()))
+
+heap = []
+figs = [False] * (n+1)
+
+def getStudentIndex(studentNum):
+    if not heap: return -1
+
+    for i in range(len(heap)):
+        if heap[i][2] == studentNum:
+            return i
+
+    return -1
+
+def updateStudent(studentIdx):
+    heap[studentIdx][0] += 1
+    heapify(heap)
+
+def isFull(): return len(heap) == n
+def addStudent(time, studentNum): heappush(heap, [1, time, studentNum])
+
+for time, studentNum in enumerate(recommends):
+    if not heap:
+        addStudent(time,studentNum)
+    else:
+        studentIdx = getStudentIndex(studentNum)
+        if studentIdx != -1:
+            updateStudent(studentIdx)
+        else:
+            if isFull(): heappop(heap)
+            addStudent(time, studentNum)
+
+
+ans = sorted([fig[2] for fig in heap])
+print(*ans)
