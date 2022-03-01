82cba37d825ca3a84e8a72b4fbccecb476b5d22a
Jaeuk Lim <applewodnr@naver.com>
2022-03-01 14:12:14 +0900

    sol: boj-dp-11722

---
+++ categories/dynamic_programming/boj-dp-11722.py
@@ -0,0 +1,15 @@
+from bisect import bisect_left
+
+n = int(input())
+arr = list(map(int,input().split()))
+
+def get_lds_length(arr):
+    dp = [arr[-1]]
+
+    for i,x in enumerate(arr[::-1][1:]):
+        if dp[-1] < x: dp.append(x)
+        else: dp[bisect_left(dp, x)] = x
+
+    return len(dp)
+
+print(get_lds_length(arr))
